05fbb36bf2b34da09ece40d70ff3ad4640d86205
Zara2ctra <zara2ctra@gmail.com>
2023-08-18 15:58:52 +0400

    init: init server

---
+++ handlers/messageHandlers.js
@@ -0,0 +1,21 @@
+const Message = require("../models/models.js");
+
+module.exports = (io, socket) => {
+
+    const getMessages = async () => {
+        const messages = await Message.findAll();
+        let data = messages.map((i) => {
+            return i.dataValues
+        })
+        console.log(data)
+        io.in(socket.sharedRoom).emit('messages', data);
+    }
+
+    const addMessage = async (message) => {
+        await Message.create({messageText: message.messageText, tagList: message.tagList});
+        await getMessages();
+    }
+
+    socket.on('message:get', getMessages)
+    socket.on('message:add', addMessage)
+}
